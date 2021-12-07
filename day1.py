6e7678157c14615ddc10a8e89796d6772922a4fe
JessicaHsu <everydayniceday@gmail.com>
2021-12-08 00:07:45 +0800

    add day1 part2
    	modified:   day1.py

--- day1.py
+++ day1.py
@@ -26,23 +26,23 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-def get_result(report):
+def get_day1_part1_result(report):
     counter = 0
     for i in report:
         if int(i[0]) < int(i[1]):
             counter+=1
     return counter
 
-def sample_solution():
+def get_day1_part1_sample_solution():
     source_list = sample_source.split()
-    count_sample = get_result(list(pairwise(source_list)))
-    print(count_sample)
+    count_sample = get_day1_part1_result(list(pairwise(source_list)))
+    return count_sample
 
-def solution1():
-    full_data_counter = get_result(list(pairwise(read_source_from('day1_input.txt'))))
-    print(full_data_counter)
+def get_day1_part1_solution1():
+    full_data_counter = get_day1_part1_result(list(pairwise(read_source_from('day1_input.txt'))))
+    return full_data_counter
 
-def solution2():
+def get_day1_part1_solution2():
     dataList = read_source_from('day1_input.txt')
     counter = 0
     for i, d in enumerate(dataList):
@@ -51,8 +51,37 @@ def solution2():
         if right < (len(dataList) - 1):
             if int(dataList[left]) < int(dataList[right]):
                 counter+=1
-    print(counter)
+    return counter
 
-sample_solution()
-solution1()
-solution2()
+
+def groupwise(iterable):
+    a, b, c = itertools.tee(iterable, 3)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    return zip(a, b, c)
+
+def get_day1_part2_result():
+    groups = list(groupwise(read_source_from('day1_input.txt')))
+
+    counter = 0
+    end_idx = len(groups) - 1
+
+    for idx, data in enumerate(groups):
+        left_idx = idx
+        right_idx = idx + 1
+
+        if right_idx <= end_idx:
+            left_element = groups[left_idx]
+            left_sum = int(left_element[0]) + int(left_element[1]) + int(left_element[2])
+            right_element = groups[right_idx]
+            right_sum = int(right_element[0]) + int(right_element[1]) + int(right_element[2])
+
+            if left_sum < right_sum:
+                counter += 1
+    return counter
+
+print(get_day1_part1_sample_solution())
+print(get_day1_part1_solution1())
+print(get_day1_part1_solution2())
+print(get_day1_part2_result())
